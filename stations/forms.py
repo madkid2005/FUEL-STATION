ed5c89dfb194e14f37545658c3b8b1b42651ceb0
Kiarash <shahedikiarash@gmail.com>
2024-06-15 18:26:56 +0330

    make adding tanks number and it stock like nuzzles number and stock

--- stations/forms.py
+++ stations/forms.py
@@ -1,12 +1,12 @@
 from django import forms
 from django.forms import inlineformset_factory
-from .models import FuelStation, GasolineNozzle, GasNozzle
+from .models import FuelStation, GasolineNozzle, GasNozzle, GasolineTank, GasTank
 
 class FuelStationForm(forms.ModelForm):
     class Meta:
         model = FuelStation
         fields = ['name', 'gasoline_tanks', 'gasoline_nozzles', 'gas_tanks', 'gas_nozzles', 'control_period', 'product_type',
-                  'gasoline_initial_stock', 'gasoline_received', 'gasoline_tank1_stock', 'gasoline_tank2_stock', 'gasoline_electronic_sales',
+                  'gasoline_initial_stock', 'gasoline_received', 'gasoline_electronic_sales',
                   'gas_initial_stock', 'gas_received', 'gas_tank1_stock', 'gas_tank2_stock', 'diesel_electronic_sales']
 
 class GasolineNozzleForm(forms.ModelForm):
@@ -19,5 +19,26 @@ class GasNozzleForm(forms.ModelForm):
         model = GasNozzle
         fields = ['nozzle_number', 'totalizer_start', 'totalizer_end']
 
+
+class GasolineTankForm(forms.ModelForm):
+    class Meta:
+        model = GasolineTank
+        fields = ['tank_number', 'final_stock']
+
+class GasTankForm(forms.ModelForm):
+    class Meta:
+        model = GasTank
+        fields = ['tank_number', 'final_stock']
+
+
+
+
+
+
+# nozzles
 GasolineNozzleFormSet = inlineformset_factory(FuelStation, GasolineNozzle, form=GasolineNozzleForm, extra=12)
 GasNozzleFormSet = inlineformset_factory(FuelStation, GasNozzle, form=GasNozzleForm, extra=12)
+
+#tanks
+GasolineTankFormSet = inlineformset_factory(FuelStation, GasolineTank, form=GasolineTankForm, extra=0)
+GasTankFormSet = inlineformset_factory(FuelStation, GasTank, form=GasTankForm, extra=0)
--- stations/models.py
+++ stations/models.py
@@ -2,28 +2,28 @@ from django.db import models
 
 class FuelStation(models.Model):
     name = models.CharField(max_length=255) # اسم جایگاه
-    gasoline_tanks = models.PositiveIntegerField(default=0) # تعداد مخزن های بنزین
+    gasoline_tanks = models.PositiveIntegerField(default=0, null=True, blank=True) # تعداد مخزن های بنزین
     gasoline_nozzles = models.PositiveIntegerField(default=0, null=True, blank=True) # تعداد نازل های بنزین
-    gas_tanks = models.PositiveIntegerField(default=0) # تعداد 
-    gas_nozzles = models.PositiveIntegerField(default=0, null=True, blank=True)
-    control_period = models.CharField(max_length=50, choices=[('start', 'ابتدا دوره'), ('end', 'انتهای دوره')])
-    product_type = models.CharField(max_length=50, choices=[('gasoline', 'بنزین'), ('diesel', 'نفتگاز'), ('both', 'هر دو')])
+    gas_tanks = models.PositiveIntegerField(default=0) # تعداد مخازن گاز
+    gas_nozzles = models.PositiveIntegerField(default=0, null=True, blank=True)  # تعداد نازل های گاز
+    control_period = models.CharField(max_length=50, choices=[('start', 'ابتدا دوره'), ('end', 'انتهای دوره')])  # انتخاب دوره
+    product_type = models.CharField(max_length=50, choices=[('gasoline', 'بنزین'), ('diesel', 'نفتگاز'), ('both', 'هر دو')])  # انتخاب محصول
     
     # Gasoline fields
-    gasoline_initial_stock = models.PositiveIntegerField(null=True, blank=True)
-    gasoline_received = models.PositiveIntegerField(null=True, blank=True)
-    gasoline_tank1_stock = models.PositiveIntegerField(null=True, blank=True)
-    gasoline_tank2_stock = models.PositiveIntegerField(null=True, blank=True)
-    gasoline_electronic_sales = models.PositiveIntegerField(null=True, blank=True)
-    gasoline_final_stock = models.FloatField(null=True, blank=True)
+    gasoline_initial_stock = models.PositiveIntegerField(null=True, blank=True) # موجودی اول دوره بنزین
+    gasoline_received = models.PositiveIntegerField(null=True, blank=True)   # مقدار رسیده دوره بنزین
+    gasoline_tank1_stock = models.PositiveIntegerField(null=True, blank=True) # موجودی مخزن شماره ۱ بنزین
+    gasoline_tank2_stock = models.PositiveIntegerField(null=True, blank=True) # موجودی مخزن شماره ۲ بنزین
+    gasoline_electronic_sales = models.PositiveIntegerField(null=True, blank=True) # مقدار فروش الکترونیکی بنزین
+    gasoline_final_stock = models.FloatField(null=True, blank=True)   # موجودی انتهای دوره بنزین
 
     # Diesel fields
-    gas_initial_stock = models.PositiveIntegerField(null=True, blank=True)
-    gas_received = models.PositiveIntegerField(null=True, blank=True)
-    gas_tank1_stock = models.PositiveIntegerField(null=True, blank=True)
-    gas_tank2_stock = models.PositiveIntegerField(null=True, blank=True)
-    diesel_electronic_sales = models.PositiveIntegerField(null=True, blank=True)
-    gas_final_stock = models.FloatField(null=True, blank=True)
+    gas_initial_stock = models.PositiveIntegerField(null=True, blank=True) # موجودی اول دوره گاز
+    gas_received = models.PositiveIntegerField(null=True, blank=True)   # مقدار رسیده دوره گاز
+    gas_tank1_stock = models.PositiveIntegerField(null=True, blank=True) # موجودی مخزن شماره ۱ گاز
+    gas_tank2_stock = models.PositiveIntegerField(null=True, blank=True) # موجودی مخزن شماره ۲ گاز
+    diesel_electronic_sales = models.PositiveIntegerField(null=True, blank=True)  # مقدار فروش الکترونیکی گاز
+    gas_final_stock = models.FloatField(null=True, blank=True) # موجودی انتهای دوره گاز
 
     def __str__(self):
         return self.name
@@ -33,9 +33,9 @@ class FuelStation(models.Model):
 # نازل های بنزین
 class GasolineNozzle(models.Model):
     station = models.ForeignKey(FuelStation, on_delete=models.CASCADE, related_name='gasoline_nozzles_details')
-    nozzle_number = models.PositiveIntegerField()
-    totalizer_start = models.PositiveIntegerField()
-    totalizer_end = models.PositiveIntegerField()
+    nozzle_number = models.PositiveIntegerField() # شماره نازل
+    totalizer_start = models.PositiveIntegerField() # عدد ابتدا دوره نازل
+    totalizer_end = models.PositiveIntegerField()  # عدد انتها دوره نازل
 
     def __str__(self):
         return f'{self.station.name} - Nozzle {self.nozzle_number}'
@@ -45,9 +45,9 @@ class GasolineNozzle(models.Model):
 # نازل های گاز
 class GasNozzle(models.Model):
     station = models.ForeignKey(FuelStation, on_delete=models.CASCADE, related_name='gas_nozzles_details')
-    nozzle_number = models.PositiveIntegerField()
-    totalizer_start = models.PositiveIntegerField()
-    totalizer_end = models.PositiveIntegerField()
+    nozzle_number = models.PositiveIntegerField() # شماره نازل
+    totalizer_start = models.PositiveIntegerField() # عدد ابتدا دوره نازل
+    totalizer_end = models.PositiveIntegerField() # عدد انتها دوره نازل
 
     def __str__(self):
         return f'{self.station.name} - Nozzle {self.nozzle_number}'
@@ -56,11 +56,11 @@ class GasNozzle(models.Model):
 # مخزن های بنزین
 class GasolineTank(models.Model):
     station = models.ForeignKey(FuelStation, on_delete=models.CASCADE)
-    tank_number = models.IntegerField()
-    final_stock = models.FloatField()
+    tank_number = models.IntegerField() # شماره مخزن
+    final_stock = models.FloatField() # عدد انتها دوره
 
 # مخزن های گاز
 class GasTank(models.Model):
     station = models.ForeignKey(FuelStation, on_delete=models.CASCADE)
-    tank_number = models.IntegerField()
-    final_stock = models.FloatField()
+    tank_number = models.IntegerField() # شماره مخزن
+    final_stock = models.FloatField() # عدد انتها دوره
